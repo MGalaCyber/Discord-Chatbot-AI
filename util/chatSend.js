61f783a579ae83b9981fd729f847f63a0477c973
MGalaCyber <87995243+MGalaCyber@users.noreply.github.com>
2021-10-22 18:02:53 +0800

    first commit

---
+++ util/chatSend.js
@@ -0,0 +1,19 @@
+const Chat = require('clever-chat')
+require("./inline.js");
+const chatSend = async (message) => {
+    const chat = new Chat({ name: "AI Chatbot", gender: "female", developer_name: "GalaXd#9165", user: message.author.id, language: "en" });
+    //get more customisations at https://npmjs.com/package/clever-chat. TypeScript supported.
+   
+      message.channel.startTyping()
+      let reply = await chat.chat(message.content).then(reply => {
+          message.sendInline(reply, { allowedMentions: { repliedUser: false } }); 
+          //you can set it to true!
+          message.channel.stopTyping()
+          
+      })
+console.log(reply)
+}
+
+module.exports = {
+    chatSend
+};
