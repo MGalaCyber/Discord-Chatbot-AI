2e92c3e3e54f6fd5830da2f9cb112bdddad278bc
MGalaCyber <87995243+MGalaCyber@users.noreply.github.com>
2021-10-25 12:50:20 +0800

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,8 +1,15 @@
 module.exports.run = async(client, message, args) => {
     let db = client.db
     
-    message.channel.send(client.em('Thanks for Invite me :)', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=869755197046530060&permissions=515466853952&scope=bot) Click here for invite!`, `🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB) Click here for Join to My Discord Server!`))
+    const inviteEmbed = new Discord.MessageEmbed()
+    .setTitle('Thanks for Invite me :)')
+    .addField('🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=869755197046530060&permissions=515466853952&scope=bot)', 'Click here for invite!')
+    .addField('🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB)', 'Click here for Join to My Discord Server!')
+
+    message.channel.send(inviteEmbed);
+    // message.channel.send(client.em('Thanks for Invite me :)', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=869755197046530060&permissions=515466853952&scope=bot) Click here for invite!` `🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB) Click here for Join to My Discord Server!`))
     }
+
     module.exports.config = {
       name: 'invite',
       aliases: ['add']
