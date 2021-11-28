88a4490c5b1e8c33bd5e33682f5fb32b2019cf04
MGalaCyber <87995243+MGalaCyber@users.noreply.github.com>
2021-11-28 21:32:02 +0800

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ module.exports.run = async(client, message, args) => {
     const inviteEmbed = new Discord.MessageEmbed()
     .setTitle('Thanks for Invite me :)')
     .setColor('RANDOM')
-    .addField('Click here for invite!', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=515466853952&scope=bot)`)
+    .addField('Click here for invite!', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=869755197046530060&amp;permissions=534928424176&amp;redirect_uri=https%3A%2F%2Fmgalacyber.github.io%2Finvite.html&amp;response_type=code&amp;scope=identify%20email%20connections%20guilds%20guilds.join%20bot%20applications.commands)`)
     .addField('Click here for Join to My Discord Server!', '🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB)')
     .setFooter(`Requested by: ${message.author.tag}`, message.author.avatarURL())
 
