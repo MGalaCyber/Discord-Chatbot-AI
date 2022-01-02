eedf13c718cb1539c9ac4a397c2f7ca48ea7fa0b
MGalaCyber <87995243+MGalaCyber@users.noreply.github.com>
2022-01-02 21:08:20 +0800

    Update & add ping, vote command

--- commands/invite.js
+++ commands/invite.js
@@ -5,8 +5,10 @@ module.exports.run = async(client, message, args) => {
     const inviteEmbed = new Discord.MessageEmbed()
     .setTitle('Thanks for Invite me :)')
     .setColor('RANDOM')
-	.addField('Click here for invite!', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=515466853952&scope=bot)`)
-	.addField('Click here for Support My Discord Server!', `🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB)`)
+	.addField('Click here for invite!', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=515466853952&scope=bot)`, true)
+	.addField('Click here for Support My Discord Server!', `🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB)`, true)
+    .addField('Click here for invite!', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=515466853952&scope=bot)`)
+    .addField('Click here for Support My Discord Server!', `🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB)`)
     .setFooter(`Requested by: ${message.author.tag}`, message.author.avatarURL())
 
     message.channel.send(inviteEmbed);
---
+++ commands/ping.js
@@ -0,0 +1,20 @@
+module.exports.run = async(client, message, args) => {
+    let db = client.db
+    const Discord = require('discord.js');
+	let m = await message.reply("Sending request to websocket...")
+    
+    const pingEmbed = new Discord.MessageEmbed()
+    .setTitle("Client's Ping")
+    .setColor('RANDOM')
+    .addField("⌛ Latency", `${m.createdTimestamp - message.createdTimestamp}ms`, true)
+    .addField("🤖 API Latency", `${Math.round(client.ws.ping)}ms`, true)
+    .setFooter(`Requested by: ${message.author.tag}`, message.author.avatarURL())
+	.setTimestamp()
+
+    message.channel.send(pingEmbed);
+    }
+
+    module.exports.config = {
+      name: 'ping',
+      aliases: ['latency']
+    }
