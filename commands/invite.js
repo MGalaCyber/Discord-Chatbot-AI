381335820704d6b4ffef6a0776b0ae4015726f4f
MGalaCyber <87995243+MGalaCyber@users.noreply.github.com>
2022-01-02 20:20:52 +0800

    update & add vote command

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ module.exports.run = async(client, message, args) => {
     .setTitle('Thanks for Invite me :)')
     .setColor('RANDOM')
     .addField('Click here for invite!', `🔗: [Invite bot](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=515466853952&scope=bot)`)
-    .addField('Click here for Join to My Discord Server!', '🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB)')
+    .addField('Click here for Support My Discord Server!', `🔗: [Join Discord Server](https://discord.gg/invite/VzGNhtmmfB)`)
     .setFooter(`Requested by: ${message.author.tag}`, message.author.avatarURL())
 
     message.channel.send(inviteEmbed);
---
+++ commands/vote.js
@@ -0,0 +1,19 @@
+module.exports.run = async(client, message, args) => {
+    let db = client.db
+    const Discord = require('discord.js');
+    
+    const voteEmbed = new Discord.MessageEmbed()
+    .setTitle('Thanks for Voted me :)')
+    .setColor('RANDOM')
+    .addField('Top.gg', `🔗: [Click here!](https://top.gg/bot/869755197046530060/vote)`)
+	.addField('Discordbotlist.com', `🔗: [Click here!](https://discordbotlist.com/bots/siesta-chan/upvote)`)
+	.addField('Discord.boats', `🔗: [Click here!](https://discord.boats/bot/869755197046530060/vote)`)
+    .setFooter(`Requested by: ${message.author.tag}`, message.author.avatarURL())
+
+    message.channel.send(voteEmbed);
+    }
+
+    module.exports.config = {
+      name: 'vote',
+      aliases: ['voted']
+    }
