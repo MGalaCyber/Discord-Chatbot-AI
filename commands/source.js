0f714c718cd7c331d2b6045543ee04b4c2772e6d
MGalaCyber <87995243+MGalaCyber@users.noreply.github.com>
2022-01-14 19:23:57 +0800

    Update source.js

--- commands/source.js
+++ commands/source.js
@@ -8,7 +8,7 @@ module.exports.run = async(client, message, args) => {
     .setColor('RANDOM')
     .addField('Click here for Source Code!', `🔗: [Click Here](https://github.com/MGalaCyber/Chatbot-AI-v1)`, true)
     .addField('Click here for Support My Discord Server!', `🔗: [Join Discord Server](https://discord.gg/2UshYsFfCP)`, true)
-    .addField('NOTICE!', `This source code is public, it is forbidden to sell and buy this code, if you want to make a video tutorial or other things using this source code, please give credit from the owner, it is forbidden to change the content of the code, remove the credit in the code! All rights reserved!`)
+    .addField('NOTICE!', `This source code is public, it is forbidden to sell and buy this code, if you want to make a video tutorial or other things using this source code, please give credit from the owner, it is forbidden to change the content of the code, remove the credit in the code! All Rights Reserved!`)
     .setImage('https://cdn.discordapp.com/attachments/891317640763695134/931169337488838676/Siesta-chan.gif')
     .setFooter(`Requested by: ${message.author.tag} | © ${author} - Siesta v${version}`, message.author.avatarURL())
 
