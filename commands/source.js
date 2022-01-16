818948e450cd8315777a92e2050b2925d238e4a6
MGalaCyber <87995243+MGalaCyber@users.noreply.github.com>
2022-01-16 19:33:15 +0800

    Update source.js

--- commands/source.js
+++ commands/source.js
@@ -6,7 +6,7 @@ module.exports.run = async(client, message, args) => {
     const sourceEmbed = new Discord.MessageEmbed()
     .setTitle('Source Code Siesta chan Chatbot')
     .setColor('RANDOM')
-    .addField('Click here for Source Code!', `🔗: [Click Here](https://github.com/MGalaCyber/Chatbot-AI-v1)`, true)
+    .addField('Click here for Source Code!', `🔗: [Click Here](https://github.com/MGalaCyber/Discord-Chatbot-AI)`, true)
     .addField('Click here for Support My Discord Server!', `🔗: [Join Discord Server](https://discord.gg/2UshYsFfCP)`, true)
     .addField('NOTICE!', `This source code is public, it is forbidden to sell and buy this code, if you want to make a video tutorial or other things using this source code, please give credit from the owner, it is forbidden to change the content of the code, remove the credit in the code! All Rights Reserved!`)
     .setImage('https://cdn.discordapp.com/attachments/891317640763695134/931169337488838676/Siesta-chan.gif')
